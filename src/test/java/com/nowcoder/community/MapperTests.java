022fbf3cfb22937ae33e50d1fd460b77f87af9d7
sleep <602563146@qq.com>
2020-12-02 23:44:57 +0800

    entity-dao-mapper-test完成数据库操作

---
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -0,0 +1,60 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+    @Autowired
+    private UserMapper userMapper;/*dao里的usermapper*/
+
+    @Test
+    public void testSelectUser() {
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser() {
+        /*id自动生成，另外有两个没写的默认值为0*/
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");/*密码末尾加入的乱码*/
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("http://www.nowcoder.com/101.png");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);/*运行2次均为1.rows为操作的行数*/
+        System.out.println(user.getId());/*运行两次150，151*/
+    }
+
+    @Test
+    public void updateUser() {
+        int rows = userMapper.updateStatus(150, 1);
+        System.out.println(rows);
+
+        rows = userMapper.updateHeader(150, "http://www.nowcoder.com/102.png");
+        System.out.println(rows);
+
+        rows = userMapper.updatePassword(150, "hello");
+        System.out.println(rows);
+    }
+}
