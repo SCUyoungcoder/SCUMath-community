6d120761358c495631f1c840d6dd749a3177e1c6
sleep <602563146@qq.com>
2020-12-05 23:25:22 +0800

    打通前后端

---
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+    @RequestMapping(path = "/student" , method = RequestMethod.POST)
+    @ResponseBody
+    public Date findtime(int id){
+        Date u = userService.findbyid(id).getCreateTime();
+        System.out.printf(u.toString());
+        return u;
+    }
+}
---
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -0,0 +1,19 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+    public User findbyid(int id){
+        return userMapper.selectById(id);
+    }
+
+
+}
