ed3a03cf257b8343e41bb3276937bf0e8a6dfdde
sleep <602563146@qq.com>
2020-12-20 23:56:13 +0800

    发送邮件

---
+++ src/main/java/com/nowcoder/community/controller/LoginController4.java
@@ -0,0 +1,11 @@
+package com.nowcoder.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController4 {
+
+    public String getRegisterPage(){
+        return "/register.html";
+    }
+}
