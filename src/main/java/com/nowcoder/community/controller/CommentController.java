ebd4cccdb2b909790a27341ce45407c8ec1b5ab7
sleep <602563146@qq.com>
2021-02-16 14:51:19 +0800

    未修改css样式的version 1.0全联通

--- src/main/java/com/nowcoder/community/controller/CommentController.java
+++ src/main/java/com/nowcoder/community/controller/CommentController.java
@@ -6,7 +6,6 @@ import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.CommentService;
 import com.nowcoder.community.service.PaperOfClassService;
 import com.nowcoder.community.service.UserService;
-import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,7 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
 
@@ -35,12 +33,9 @@ public class CommentController {
     //@ResponseBody
     public String addDiscussPost(@PathVariable("paperid") int paperid, Comment comment) {
         User user = hostHolder.getUser();
-
-
         comment.setUserid(user.getId());
         comment.setCreatetime(new Date());
         commentService.addComment(comment);
-
         return "redirect:/detail?id="+paperid;
     }
     @LoginRequired
--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -21,13 +21,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.net.URLEncoder;
 import java.util.*;
 
 @Controller
